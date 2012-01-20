521a5b8fb6ae3e912a72aad3c31172ff4dbe48a1
DellLaptop <first.team.2474@gmail.com>
2012-01-20 06:37:10 -0600

    Commented out h file. Copied code from the default code example, gutted, and adapted to our robot. Works.

--- Robot2012.cpp
+++ Robot2012.cpp
@@ -1,12 +1,16 @@
-#include "Robot2012.h"
+#include "WPILib.h"
+//#include "Robot2012.h"
 #include "Vision/AxisCamera.h"
-
 ////////////////////////////////////////////////////////
 // Defines and typedefs
 ////////////////////////////////////////////////////////
 #define ANALOG_OUTPUT_CHANNEL 1		//in 2012 this should be in slot 1 (chan 1), or slot 5 (chan 2)
-#define DIGITAL_OUTPUT_CHANNEL 2	//in 2012 this should be in slot 3 (chan 1), or slot 7 (chan 2)
+#define DIGITAL_OUTPUT_CHANNEL 1	//in 2012 this should be in slot 3 (chan 1), or slot 7 (chan 2)
 #define PNEUMATIC_OUTPUT_CHANNEL 1	//in 2012 this should be in slot 2 (chan 1), or slot 6 (chan 2)
+
+#define AXIS_CAMERA_CONNECTED_TO_CRIO
+//TODO setup IPs for the camera based on this #define
+
 
 typedef enum
 {
@@ -21,89 +25,146 @@ typedef enum
 	PWM_CHANNEL_9_UNUSED,
 }PWM_CHANNEL_TYPE;
 
-////////////////////////////////////////////////////////
-//  Class Methods
-////////////////////////////////////////////////////////
-Robot2012::Robot2012(void):
+class Robot2012 : public IterativeRobot
+{
+	Jaguar jaguarFrontLeft;
+	Jaguar jaguarFrontRight;
+	RobotDrive myRobot; // robot drive system
+	Joystick stickRightDrive; // only joystick
+	Joystick stickLeftDrive;
+	Joystick stickShooter;
+
+	Timer timeInState;
+	Timer timeSinceBoot;
+	
+	DriverStation *driverStation;
+		
+		// Local variables to count the number of periodic loops performed
+	
+	UINT32 m_autoPeriodicLoops;
+	UINT32 m_disabledPeriodicLoops;
+	UINT32 m_telePeriodicLoops;
+		
+public:
+
+	Robot2012(void):
 		jaguarFrontLeft(DIGITAL_OUTPUT_CHANNEL,PWM_CHANNEL_1_JAGUAR_FRONT_LEFT),
 		jaguarFrontRight(DIGITAL_OUTPUT_CHANNEL,PWM_CHANNEL_2_JAGUAR_FRONT_RIGHT),
-		myRobot(&jaguarFrontLeft, &jaguarFrontRight),	// these must be initialized in the same order
-		stickRightDrive(1),		// as they are declared above
+		myRobot(&jaguarFrontLeft,&jaguarFrontRight),
+		stickRightDrive(1),
 		stickLeftDrive(2),
-		stickShooter(3)
-{
-	myRobot.SetSafetyEnabled(false);
-//	myRobot.SetExpiration(0.1);
-	//camera = *(AxisCamera::GetInstance("10.24.74.11"));
-}
+		stickShooter(3),
+		timeInState(),
+		timeSinceBoot()
+	{
+		printf("Robot2012 Constructor Started\n");
 
-void Robot2012::StartCompetition()
-{
+		// Acquire the Driver Station object
+		driverStation = DriverStation::GetInstance();
+
+		// Iterate over all the buttons on each joystick, setting state to false for each
+//		UINT8 buttonNum = 1;						// start counting buttons at button 1
+//		for (buttonNum = 1; buttonNum <= NUM_JOYSTICK_BUTTONS; buttonNum++) {
+//			m_rightStickButtonState[buttonNum] = false;
+//			m_leftStickButtonState[buttonNum] = false;
+//		}
+
+		// Initialize counters to record the number of loops completed in autonomous and teleop modes
+		m_autoPeriodicLoops = 0;
+		m_disabledPeriodicLoops = 0;
+		m_telePeriodicLoops = 0;
+
+		printf("Robot2012 Constructor Completed\n");
+	}
 	
-}
-
-void Robot2012::RobotInit()
-{
-
-	myRobot.SetSafetyEnabled(false);	
-}
-
-void Robot2012::DisabledInit()
-{
-
-	myRobot.SetSafetyEnabled(false);
-}
-
-void Robot2012::AutonomousInit()
-{
-
-	myRobot.SetSafetyEnabled(false);
-}
-
-void Robot2012::TeleopInit()
-{
-
-	myRobot.SetSafetyEnabled(false);
-//	myRobot.SetExpiration(0.1);
-//	myRobot.SetSafetyEnabled(true);
-}
-
-void Robot2012::DisabledPeriodic()
-{
 	
-}
+	/********************************** Init Routines *************************************/
 
-void Robot2012::AutonomousPeriodic()
-{
-	//AxisCamera &camera = AxisCamera::GetInstance();
-	myRobot.SetSafetyEnabled(false);
-//	myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
-//	Wait(2.0); 				//    for 2 seconds
-//	myRobot.Drive(0.0, 0.0); 	// stop robot
-}
-
-void Robot2012::TeleopPeriodic()
-{
-	//AxisCamera &camera = AxisCamera::GetInstance();
-}
-
-void Robot2012::DisabledContinuous()
-{
+	void RobotInit(void) 
+	{
+		// Actions which would be performed once (and only once) upon initialization of the
+		// robot would be put here.
+		
+		printf("RobotInit() completed.\n");
+		
+		timeSinceBoot.Start();
+	}
 	
-}
+	void DisabledInit(void) 
+	{
+		timeInState.Reset();
+		timeInState.Start();
+		m_disabledPeriodicLoops = 0;			// Reset the loop counter for disabled mode
+	}
 
-void Robot2012::AutonomousContinuous()
-{
+	void AutonomousInit(void) 
+	{
+		timeInState.Reset();
+		timeInState.Start();
+		m_autoPeriodicLoops = 0;				// Reset the loop counter for autonomous mode
+	}
+
+	void TeleopInit(void) 
+	{
+		m_telePeriodicLoops = 0;				// Reset the loop counter for teleop mode
+		timeInState.Reset();
+		timeInState.Start();
+	}
+
+	/********************************** Periodic Routines *************************************/
 	
-}
+	void DisabledPeriodic(void)  
+	{
+		// increment the number of disabled periodic loops completed
+		m_disabledPeriodicLoops++;
+	}
 
-void Robot2012::TeleopContinuous()
-{
-	myRobot.TankDrive(stickLeftDrive, stickRightDrive); // drive with arcade style (use right stick)
+	void AutonomousPeriodic(void) 
+	{
+		m_autoPeriodicLoops++;
 
-}
-///////////////////////////////////////////////////////
-// Non-class functions
-///////////////////////////////////////////////////////
+				
+		/* the below code (if uncommented) would drive the robot forward at half speed
+		 * for two seconds.  This code is provided as an example of how to drive the 
+		 * robot in autonomous mode, but is not enabled in the default code in order
+		 * to prevent an unsuspecting team from having their robot drive autonomously!
+		 */
+		/* below code commented out for safety
+		if (m_autoPeriodicLoops == 1) {
+			// When on the first periodic loop in autonomous mode, start driving forwards at half speed
+			m_robotDrive->Drive(0.5, 0.0);			// drive forwards at half speed
+		}
+		if (m_autoPeriodicLoops == (2 * GetLoopsPerSec())) {
+			// After 2 seconds, stop the robot 
+			m_robotDrive->Drive(0.0, 0.0);			// stop robot
+		}
+		*/
+	}
+
+	
+	void TeleopPeriodic(void) 
+	{
+		// increment the number of teleop periodic loops completed
+		m_telePeriodicLoops++;
+		myRobot.TankDrive(stickLeftDrive,stickRightDrive);	
+	}
+
+
+/********************************** Continuous Routines *************************************/
+	void DisabledContinuous(void) 
+	{
+	}
+
+	void AutonomousContinuous(void)	
+	{
+	}
+
+	void TeleopContinuous(void) 
+	{
+		AxisCamera &camera = AxisCamera::GetInstance("10.24.74.11");
+	}
+
+				
+};
+
 START_ROBOT_CLASS(Robot2012);
-
